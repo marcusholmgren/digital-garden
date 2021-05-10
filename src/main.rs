a6f553f2fad7c871701d85898811b6a786567328
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-05-10 12:37:36 +0200

    Documenting and writing helptext for Structopt CLI commands and flags

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use color_eyre::eyre::Result;
 use structopt::StructOpt;
 
+/// A CLI for the growing and curation of a digital garden
+///
+/// Visit https://www.rustadventure.rs/garden for more!
 #[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
 struct Opt {
     #[structopt(subcommand)]
     cmd: Command,
@@ -9,7 +13,13 @@ struct Opt {
 
 #[derive(StructOpt, Debug)]
 enum Command {
+    /// write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file to your
+    /// garden
     Write {
+        /// Optionally set a title for what you are going to write about
         #[structopt(short, long)]
         title: Option<String>
     },
